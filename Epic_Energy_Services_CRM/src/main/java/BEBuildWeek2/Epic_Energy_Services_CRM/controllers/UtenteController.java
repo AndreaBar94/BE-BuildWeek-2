a7bf04843acb7b5e5d15156736a7533aae38adbf
Luca-Sberna <sberna.luca.ibiza@gmail.com>
2023-06-19 17:25:54 +0200

    add debug for controller

--- Epic_Energy_Services_CRM/src/main/java/BEBuildWeek2/Epic_Energy_Services_CRM/controllers/UtenteController.java
+++ Epic_Energy_Services_CRM/src/main/java/BEBuildWeek2/Epic_Energy_Services_CRM/controllers/UtenteController.java
@@ -1,9 +1,9 @@
 package BEBuildWeek2.Epic_Energy_Services_CRM.controllers;
 
-import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
@@ -27,33 +27,33 @@ import BEBuildWeek2.Epic_Energy_Services_CRM.services.UtenteService;
 public class UtenteController {
 	@Autowired
 	private UtenteService utenteService;
-	
+
 	@PostMapping("")
 	@ResponseStatus(HttpStatus.CREATED)
 	public Utente saveUtente(@RequestBody @Validated UserRegistrationPayload body) {
 		return utenteService.createUtente(body);
 	}
-	
+
 	@GetMapping("")
 	public Page<Utente> getUtente(@RequestParam(defaultValue = "0") int page,
-				@RequestParam(defaultValue = "10") int size, 
-				@RequestParam(defaultValue ="id") String sortBy) {
+			@RequestParam(defaultValue = "10") int size, @RequestParam(defaultValue = "id") String sortBy) {
 		return utenteService.findAllUtenti(page, size, sortBy);
 	}
-	
+
 	@GetMapping("/{utenteId}")
-	public Optional<Utente> getUtente(@PathVariable UUID utenteId) throws Exception {
+	public Utente getUtente(@PathVariable UUID utenteId) throws Exception {
 		return utenteService.findUtenteById(utenteId);
 	}
-	
+
 	@PutMapping("/{utenteId}")
-	public Utente updateUtente(@PathVariable UUID utenteId, @RequestBody UserRegistrationPayload body) throws Exception {
+	public Utente updateUtente(@PathVariable UUID utenteId, @RequestBody UserRegistrationPayload body)
+			throws Exception {
 		return utenteService.findUtenteByIdAndUpdate(utenteId, body);
 	}
-	
+
 	@DeleteMapping("/{utenteId}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void deleteUtente(@PathVariable UUID utenteId) {
+	public void deleteUtente(@PathVariable UUID utenteId) throws NotFoundException {
 		utenteService.findUtenteByIdAndDelete(utenteId);
 	}
 }
