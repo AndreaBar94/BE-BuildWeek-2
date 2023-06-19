601cc051a7cedf03638f1511d1f8aef9197b27b7
AndreaBar94 <andreabarocchi@gmail.com>
2023-06-19 12:05:30 +0200

    project start, env.properties added to git.ignore, packages tree created

---
+++ Epic_Energy_Services_CRM/src/test/java/BEBuildWeek2/Epic_Energy_Services_CRM/EpicEnergyServicesCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package BEBuildWeek2.Epic_Energy_Services_CRM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicEnergyServicesCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
