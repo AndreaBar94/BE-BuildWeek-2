4a5763963b3760b53d7e8ca939c2c14da4345307
AndreaBar94 <andreabarocchi@gmail.com>
2023-06-21 09:46:23 +0200

    runners classes created

---
+++ Epic_Energy_Services_CRM/src/main/java/BEBuildWeek2/Epic_Energy_Services_CRM/IndirizzoRunner.java
@@ -0,0 +1,13 @@
+package BEBuildWeek2.Epic_Energy_Services_CRM;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class IndirizzoRunner implements CommandLineRunner{
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
