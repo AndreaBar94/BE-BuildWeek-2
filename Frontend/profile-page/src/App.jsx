e4870ee384bad576bb9478c679ada7623d15a577
Andrea Caforio <andreacaforio74@gmail.com>
2023-06-22 15:53:13 +0200

    css di profile page

--- Frontend/profile-page/src/App.jsx
+++ Frontend/profile-page/src/App.jsx
@@ -1,14 +1,20 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Profile from "./components/Profile";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <>
-    {/* <Login></Login> */}
-    <Profile></Profile>
-    </>
+    <div id="all">
+      <BrowserRouter>
+        <Navbar></Navbar>
+        <Routes>
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- Frontend/profile-page/src/components/profile.jsx
+++ Frontend/profile-page/src/components/profile.jsx
@@ -1,12 +1,27 @@
 const Profile = () => {
-    return(
-        <>
-            <p className="text-center mt-3">That's your Profile</p>
-            <p className="ms-3">Surname:</p>
-            <p className="ms-3">Name:</p>
-            <p className="ms-3">Email or Username:</p>
-            <p className="ms-3">Phone Number:</p>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="container">
+        <div className="text-center ms-3  mb-3 profile">That's your Profile</div>
+
+        <div placeholder="Your surname" className="d-flex miniCont">
+          <div className="text-center ms-3 text">Surname:</div>
+          <input className="inputText " type="text" />
+        </div>
+        <div placeholder="Your name" className="d-flex miniCont">
+          <div className="text-center ms-3 text">Name:</div>
+          <input className="inputText " type="text" />
+        </div>
+        <div placeholder="Your email" className="d-flex miniCont">
+          <div className="text-center ms-3 text">Email or username</div>
+          <input className="inputText " type="email" />
+        </div>
+        <div placeholder="Your phone number" className="d-flex miniCont">
+          <div className="text-center ms-3 text">Phone number</div>
+          <input className="inputText " type="tel" />
+        </div>
+      </div>
+    </>
+  );
+};
 export default Profile;
