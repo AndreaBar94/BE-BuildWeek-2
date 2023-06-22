eaffad4d0dccb270948fea9efcdaf08b76bd22ba
DennisLoiak11 <122790628+DennisLoiak11@users.noreply.github.com>
2023-06-22 15:34:26 +0200

    routes profile

--- Frontend/profile-page/src/components/profile.jsx
+++ Frontend/profile-page/src/components/profile.jsx
@@ -2,10 +2,10 @@ const Profile = () => {
     return(
         <>
             <p className="text-center mt-3">That's your Profile</p>
-            <p className="ms-3">Surname:</p>
-            <p className="ms-3">Name:</p>
-            <p className="ms-3">Email or Username:</p>
-            <p className="ms-3">Phone Number:</p>
+            <p className="ms-3">Surname: </p>
+            <p className="ms-3">Name: </p>
+            <p className="ms-3">Email or Username: </p>
+            <p className="ms-3">Phone Number: </p>
         </>
     )
 }
