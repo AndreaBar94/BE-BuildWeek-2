d3db7ac84eb951bd7929e1eae7ba3567606f34b6
Andrea Caforio <andreacaforio74@gmail.com>
2023-06-22 14:51:38 +0200

    Navbar aggiunta come componente

---
+++ Frontend/profile-page/src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Navbar = () => {
+  const [isSettingsOpen, setIsSettingsOpen] = useState(false);
+
+  const toggleSettings = () => {
+    setIsSettingsOpen(!isSettingsOpen);
+  };
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light px-4">
+      <Link className="navbar-brand" to="/">
+        Home
+      </Link>
+
+      <button className="navbar-toggler" type="button" onClick={toggleSettings}>
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className={`collapse navbar-collapse ${isSettingsOpen ? "show" : ""}`}>
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link className="nav-link" to="/search">
+              Search
+            </Link>
+          </li>
+        </ul>
+
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link className="nav-link" to="/profile">
+              Profile
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
