11c7d5e2f0f23724c5f5870f308c07d53f2da0d5
DennisLoiak11 <122790628+DennisLoiak11@users.noreply.github.com>
2023-06-22 16:45:40 +0200

    fix navbar, homepage

--- Frontend/profile-page/src/components/Home.jsx
+++ Frontend/profile-page/src/components/Home.jsx
@@ -1,7 +1,21 @@
 const Home = () => {
     return(
         <>
-        <p className="text-center mt-3 fs-3 text-white">QUESTA E' LA HOME PAGE</p>
+
+        <div className="d-flex justify-content-around my-5">
+        <section className="mx-5">
+            <div className="fs-5">Fatture</div>
+        </section>
+
+        <section className="mx-5">
+            <div className="fs-5">Utenti</div>
+        </section>
+
+        <section className="mx-5">
+            <div className="fs-5">Clienti</div>
+        </section>
+        </div>
+       
         </>
     )
 }
--- Frontend/profile-page/src/components/Navbar.jsx
+++ Frontend/profile-page/src/components/Navbar.jsx
@@ -10,33 +10,16 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light px-4">
-      <Link className="navbar-brand" to="/home">
+  <div className="text-center mb-5">
+        <Link className="navbar-brand p-3 me-3" to="/home">
         Home
-      </Link>
+        </Link>
 
-      <button className="navbar-toggler" type="button" onClick={toggleSettings}>
-        <span className="navbar-toggler-icon"></span>
-      </button>
+        <Link className="navbar-brand p-3" to="/profile">
+        Profile
+        </Link>
+  </div>
 
-      <div className={`collapse navbar-collapse ${isSettingsOpen ? "show" : ""}`}>
-        <ul className="navbar-nav ms-2 me-3">
-          <li className="nav-item">
-            <Link className="nav-link" to="/search">
-              Search
-            </Link>
-          </li>
-        </ul>
-
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link className="nav-link" to="/profile">
-              Profile
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
   );
 };
 
