5f595c7319e2e2b42ce4bfaeb7f150ff40d4aa9e
Luca-Sberna <sberna.luca.ibiza@gmail.com>
2023-06-23 10:21:35 +0200

    added login validations

--- Epic_Energy_Services_CRM/src/main/java/BEBuildWeek2/Epic_Energy_Services_CRM/payloads/UserRegistrationPayload.java
+++ Epic_Energy_Services_CRM/src/main/java/BEBuildWeek2/Epic_Energy_Services_CRM/payloads/UserRegistrationPayload.java
@@ -1,22 +1,25 @@
 package BEBuildWeek2.Epic_Energy_Services_CRM.payloads;
-import jakarta.validation.constraints.*;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UserRegistrationPayload {
 	@Size(max = 20, message = "Username must be at most 20 characters long")
-    private String username;
+	private String username;
 
-    @NotEmpty(message = "Name is required")
-    private String name;
+	@NotEmpty(message = "Name is required")
+	private String name;
 
-    @NotEmpty(message = "Surname is required")
-    private String surname;
+	@NotEmpty(message = "Surname is required")
+	private String surname;
 
-    @Email(message = "Invalid email format")
-    private String emailUtente;
+	@Email(message = "Invalid email format")
+	private String emailUtente;
 
-    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$",
-            message = "Password must be at least 8 characters long and contain at least one digit, one letter, and one special character")
-    private String password;
+	@Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$", message = "Password must be at least 8 characters long and contain at least one digit, one letter, and one special character")
+	private String password;
 }
