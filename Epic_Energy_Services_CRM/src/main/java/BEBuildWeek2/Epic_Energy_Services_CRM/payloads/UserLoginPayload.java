07a1373bd10dd0fd7ff7fec6d7ba8b6051760d9e
Pierluigi Monzon <pierlui-gi@hotmail.com>
2023-06-23 10:33:58 +0200

    UserLogin validation

--- Epic_Energy_Services_CRM/src/main/java/BEBuildWeek2/Epic_Energy_Services_CRM/payloads/UserLoginPayload.java
+++ Epic_Energy_Services_CRM/src/main/java/BEBuildWeek2/Epic_Energy_Services_CRM/payloads/UserLoginPayload.java
@@ -1,9 +1,15 @@
 package BEBuildWeek2.Epic_Energy_Services_CRM.payloads;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
 public class UserLoginPayload {
-	String emailUtente;
-	String password;
+	@Email(message = "Invalid email format")
+	private String emailUtente;
+	
+	@Pattern(regexp = "^(?=.[0-9])(?=.[a-zA-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$" , 
+			message = "Password must be at least 8 characters long and contain at least one digit, one letter, and one special character")
+	private String password;
 }
