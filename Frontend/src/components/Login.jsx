a21ebcee15688a473000f4587db26d9c514d46ab
Luca-Sberna <sberna.luca.ibiza@gmail.com>
2023-06-23 13:55:43 +0200

    adjust graphics

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,97 +1,98 @@
-import React, { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Link, Navigate, useNavigate } from 'react-router-dom';
-
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import logo from "../logo.svg";
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9";
 
 const RegistrationForm = () => {
-        const [loginError, setLoginError] = useState(false);
-        const [isLoggedIn, setLoggedIn] = useState(false);
-        const [formData, setFormData] = useState({
-            emailUtente: '',
-            password: ''
-        });
+  const [loginError, setLoginError] = useState(false);
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  const [formData, setFormData] = useState({
+    emailUtente: "",
+    password: "",
+  });
 
-        const navigate = useNavigate();
+  const navigate = useNavigate();
 
-        const handleInputChange = (event) => {
-            setFormData({
-            ...formData,
-            [event.target.name]: event.target.value,
-            });
-        };
+  const handleInputChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
 
-        const handleLoginSubmit = (event) => {
-            event.preventDefault();
+  const handleLoginSubmit = (event) => {
+    event.preventDefault();
 
-        fetch('http://localhost:3142/auth/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: 'Bearer ' + token,
-        },
-        body: JSON.stringify(formData),
-        })
-        .then((response) => response.json())
-        .then((data) => {
-            if (data.ok) {
-                setLoggedIn(true);
-                navigate("/Home");
-            } else {
-            setLoginError(true);
-            }
-        })
-        .catch((error) => {
-            console.error('Login error:', error);
-            setLoginError(true);
-        });
-    };
+    fetch("http://localhost:3142/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.ok) {
+          setLoggedIn(true);
+          navigate("/Home");
+        } else {
+          setLoginError(true);
+        }
+      })
+      .catch((error) => {
+        console.error("Login error:", error);
+        setLoginError(true);
+      });
+  };
 
-    
+  if (isLoggedIn === true) {
+    return <Navigate to="/Home" />;
+  }
 
-    if(isLoggedIn === true) {
-                    return <Navigate to="/Home" />
-                };
-
-    return (
-        <div className="container">
-        
-        <div className="container">
+  return (
+    <div className="container mt-4 p-3 border border-3 border-primary rounded-4 shadow">
+      <div className="d-flex justify-content-between">
         <h2>Login</h2>
-        {/* {loginError && <p>Login failed. Please try again.</p>} */}
-        <form onSubmit={handleLoginSubmit}>
-            <div className="form-group">
-            <label>Email:</label>
-            <input
-                type="email"
-                className="form-control"
-                name="emailUtente"
-                placeholder="Enter email"
-                value={formData.emailUtente}
-                onChange={handleInputChange}
-            />
-            </div>
-            <div className="form-group">
-            <label>Password:</label>
-            <input
-                type="password"
-                className="form-control"
-                name="password"
-                placeholder="Enter password"
-                value={formData.password}
-                onChange={handleInputChange}
-            />
-            </div>
-                <button type="submit" className="btn btn-primary mt-2">
-            Login
-            </button>
-            <p>
-            Don't have an account? <Link to="/SignIn">Register</Link>
-            </p>
-        </form>
+        <img className="logo " src={logo} alt="logo" />
+      </div>
+      {/* {loginError && <p>Login failed. Please try again.</p>} */}
+      <form onSubmit={handleLoginSubmit}>
+        <div className="form-group">
+          <label>Email:</label>
+          <input
+            type="email"
+            className="form-control shadow mb-3"
+            name="emailUtente"
+            placeholder="Enter email"
+            value={formData.emailUtente}
+            onChange={handleInputChange}
+          />
         </div>
+        <div className="form-group">
+          <label>Password:</label>
+          <input
+            type="password"
+            className="form-control shadow"
+            name="password"
+            placeholder="Enter password"
+            value={formData.password}
+            onChange={handleInputChange}
+          />
         </div>
-    );
+        <button type="submit" className="btn-form btn btn-primary mt-3 shadow">
+          Login
+        </button>
+        <p className="mt-4">
+          Don't have an account?{" "}
+          <Link className="text-decoration-none fw-bold" to="/SignIn">
+            Register
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
 };
 
 export default RegistrationForm;
--- Frontend/src/components/SignIn.jsx
+++ Frontend/src/components/SignIn.jsx
@@ -1,15 +1,16 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import logo from "../logo.svg";
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9";
 
 const SignIn = () => {
   const [signIn, setSignIn] = useState(false);
   const [formData, setFormData] = useState({
-    username: '',
-    name: '',
-    surname: '',
-    emailUtente: '',
-    password: '',
+    username: "",
+    name: "",
+    surname: "",
+    emailUtente: "",
+    password: "",
   });
 
   const handleInputChange = (event) => {
@@ -31,103 +32,111 @@ const SignIn = () => {
     event.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:3142/auth/registration', {
-        method: 'POST',
+      const response = await fetch("http://localhost:3142/auth/registration", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
-          Authorization: 'Bearer ' + token,
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token,
         },
         body: JSON.stringify(formData),
       });
       const data = await response.json();
 
       if (response.status === 201) {
-        alert('Registration successful!');
-        console.log('Registration data: ' + data);
+        alert("Registration successful!");
+        console.log("Registration data: " + data);
       } else {
-        alert('Error occurred with registration!');
+        alert("Error occurred with registration!");
       }
     } catch (error) {
-      console.error('Registration error:', error);
+      console.error("Registration error:", error);
     }
   };
 
   return (
     <>
-    <div className="container">
-      <h2>Registration</h2>
-      <form onSubmit={handleRegistrationSubmit}>
-        <div className="form-group">
-          <label>Username:</label>
-          <input
-          required
-            type="text"
-            className="form-control"
-            name="username"
-            placeholder="Enter username"
-            value={formData.username}
-            onChange={handleInputChange}
-          />
+      <div className="container mt-4 p-3 border border-3 border-primary rounded-4 shadow">
+        <div className="d-flex justify-content-between">
+          <h2>Registration</h2>
+          <img className="logo " src={logo} alt="logo" />
         </div>
-        <div className="form-group">
-          <label>Name:</label>
-          <input
-          required
-            type="text"
-            className="form-control"
-            name="name"
-            placeholder="Enter name"
-            value={formData.name}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div className="form-group">
-          <label>Surname:</label>
-          <input
-          required
-            type="text"
-            className="form-control"
-            name="surname"
-            placeholder="Enter surname"
-            value={formData.surname}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div className="form-group">
-          <label>Email:</label>
-          <input
-          required
-            type="email"
-            className="form-control"
-            name="emailUtente"
-            placeholder="Enter email"
-            value={formData.emailUtente}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div className="form-group">
-          <label>Password:</label>
-          <input
-          required
-            type="password"
-            className="form-control"
-            name="password"
-            placeholder="Enter password (at least 8 characters, one digit, one letter, and one special character)"
-            value={formData.password}
-            onChange={handleInputChange}
-          />
-        </div>
-        <button type="submit" className="btn btn-primary mt-2">
-          Register
-        </button>
-      </form>
-      <p>
-          Return to <Link to="/">Login Page</Link>
+        <form onSubmit={handleRegistrationSubmit}>
+          <div className="form-group">
+            <label>Username:</label>
+            <input
+              required
+              type="text"
+              className="form-control shadow mb-3"
+              name="username"
+              placeholder="Enter username"
+              value={formData.username}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="form-group">
+            <label>Name:</label>
+            <input
+              required
+              type="text"
+              className="form-control shadow mb-3"
+              name="name"
+              placeholder="Enter name"
+              value={formData.name}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="form-group">
+            <label>Surname:</label>
+            <input
+              required
+              type="text"
+              className="form-control shadow mb-3"
+              name="surname"
+              placeholder="Enter surname"
+              value={formData.surname}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="form-group">
+            <label>Email:</label>
+            <input
+              required
+              type="email"
+              className="form-control shadow mb-3"
+              name="emailUtente"
+              placeholder="Enter email"
+              value={formData.emailUtente}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="form-group">
+            <label>Password:</label>
+            <input
+              required
+              type="password"
+              className="form-control shadow "
+              name="password"
+              placeholder="Enter password (at least 8 characters, one digit, one letter, and one special character)"
+              value={formData.password}
+              onChange={handleInputChange}
+            />
+          </div>
+          <button
+            type="submit"
+            className="btn-form btn btn-primary shadow mt-3"
+          >
+            Register
+          </button>
+        </form>
+        <p className="mt-4">
+          Return to{" "}
+          <Link className="text-decoration-none fw-bold " to="/">
+            Login Page
+          </Link>
         </p>
       </div>
     </>
-    
-  )
-}
+  );
+};
 
 export default SignIn;
