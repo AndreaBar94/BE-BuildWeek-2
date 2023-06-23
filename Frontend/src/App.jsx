c2292eff98a0d9666cafcda4a36945c7abb896a4
AndreaBar94 <andreabarocchi@gmail.com>
2023-06-23 08:59:34 +0200

    cors filter ok, registration method ok

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,19 +1,42 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useState } from "react";
 import Login from "./components/Login";
 import Profile from "./components/profile";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    // Effettua l'accesso e imposta isLoggedIn a true
+    setIsLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    // Effettua il logout e imposta isLoggedIn a false
+    setIsLoggedIn(false);
+  };
+
   return (
     <div id="all">
       <BrowserRouter>
-        <Navbar></Navbar>
+        <Navbar isLoggedIn={isLoggedIn} handleLogout={handleLogout} />
         <Routes>
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Login handleLogin={handleLogin} />} />
+          {isLoggedIn ? (
+            <>
+              {/* <Route path="/profile" element={<Profile />} />
+              <Route path="/home" element={<Home />} /> */}
+            </>
+          ) : (
+            <Route
+              path="/*"
+              element={<Navigate to="/" replace={true} />}
+            />
+          )}
         </Routes>
       </BrowserRouter>
     </div>
---
+++ Frontend/src/components/Login.jsx
@@ -0,0 +1,148 @@
+import React, { useState } from 'react';
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9';
+const RegistrationForm = () => {
+    const [formData, setFormData] = useState({
+      username: '',
+      name: '',
+      surname: '',
+      email: '',
+      password: '',
+    });
+
+  const handleInputChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleRegistrationSubmit = (event) => {
+    event.preventDefault();
+
+    fetch('http://localhost:3142/auth/registration', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle the registration response data
+        alert('registration ok!')
+        console.log('Registration data:', data);
+      })
+      .catch((error) => {
+        // Handle registration errors
+        console.error('Registration error:', error);
+      });
+  };
+
+  const handleLoginSubmit = (event) => {
+    event.preventDefault();
+
+    fetch('/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle the login response data
+        console.log('Login data:', data);
+      })
+      .catch((error) => {
+        // Handle login errors
+        console.error('Login error:', error);
+      });
+  };
+
+  return (
+    <div>
+     <h2>Registration</h2>
+      <form onSubmit={handleRegistrationSubmit}>
+        <label>
+          Username:
+          <input
+            type="text"
+            name="username"
+            value={formData.username}
+            onChange={handleInputChange}
+          />
+        </label>
+        <br />
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleInputChange}
+          />
+        </label>
+        <br />
+        <label>
+          Surname:
+          <input
+            type="text"
+            name="surname"
+            value={formData.surname}
+            onChange={handleInputChange}
+          />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleInputChange}
+          />
+        </label>
+        <br />
+        <button type="submit">Register</button>
+      </form>
+
+      <h2>Login</h2>
+      <form onSubmit={handleLoginSubmit}>
+        <label>
+          Username:
+          <input
+            type="text"
+            name="username"
+            value={formData.username}
+            onChange={handleInputChange}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleInputChange}
+          />
+        </label>
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default RegistrationForm;
