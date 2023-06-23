9588de9dc727c65a2167c3deee7487d91ca25850
AndreaBar94 <andreabarocchi@gmail.com>
2023-06-23 09:36:14 +0200

    validation added on pom.xml, validation on registration payload

--- Epic_Energy_Services_CRM/src/main/java/BEBuildWeek2/Epic_Energy_Services_CRM/payloads/UserRegistrationPayload.java
+++ Epic_Energy_Services_CRM/src/main/java/BEBuildWeek2/Epic_Energy_Services_CRM/payloads/UserRegistrationPayload.java
@@ -1,8 +1,22 @@
 package BEBuildWeek2.Epic_Energy_Services_CRM.payloads;
-
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
 public class UserRegistrationPayload {
-	String username, name, surname, emailUtente, password;
+	@Size(max = 20, message = "Username must be at most 20 characters long")
+    private String username;
+
+    @NotEmpty(message = "Name is required")
+    private String name;
+
+    @NotEmpty(message = "Surname is required")
+    private String surname;
+
+    @Email(message = "Invalid email format")
+    private String emailUtente;
+
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$",
+            message = "Password must be at least 8 characters long and contain at least one digit, one letter, and one special character")
+    private String password;
 }
