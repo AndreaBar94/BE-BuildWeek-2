6739b5856633af2e7441370fd1c3a107e0a54de7
AndreaBar94 <andreabarocchi@gmail.com>
2023-06-23 14:57:37 +0200

    fatture tables structure

--- Frontend/src/components/Fatture.jsx
+++ Frontend/src/components/Fatture.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 const Fatture = () => {
-  return (
-    <div>Fatture</div>
-  )
-}
+  const [fatture, setFatture] = useState([]);
 
-export default Fatture
+  useEffect(() => {
+    fetchFatture();
+  }, []);
+
+  const fetchFatture = async () => {
+    try {
+      const response = await fetch('API_URL'); // Inserisci l'URL corretto per ottenere i dati delle fatture
+      const data = await response.json();
+      setFatture(data);
+    } catch (error) {
+      console.error('Errore durante il recupero delle fatture:', error);
+    }
+  };
+
+  return (
+    <div>
+      <div className="row">
+        <div className="col">ID</div>
+        <div className="col">Anno</div>
+        <div className="col">Data</div>
+        <div className="col">Importo</div>
+        <div className="col">Numero Fattura</div>
+        <div className="col">Stato</div>
+        <div className="col">ID Cliente</div>
+      </div>
+      {fatture.map((fattura) => (
+        <div key={fattura.id} className="row">
+          <div className="col">{fattura.id}</div>
+          <div className="col">{fattura.anno}</div>
+          <div className="col">{fattura.data}</div>
+          <div className="col">{fattura.importo}</div>
+          <div className="col">{fattura.numeroFattura}</div>
+          <div className="col">{fattura.stato}</div>
+          <div className="col">{fattura.idCliente}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Fatture;
