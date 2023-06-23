b02373cac0190d2eab5dab7b9b313c6ac17eb74e
AndreaBar94 <andreabarocchi@gmail.com>
2023-06-23 08:34:49 +0200

    deleted profile-page folder

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,7 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
-import Profile from "./components/Profile";
+import Profile from "./components/profile";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 
